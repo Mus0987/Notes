ffe9c24821331b873ffcf73ea712e517f81840b9
MuskanGupta <108206688+Mus0987@users.noreply.github.com>
2023-09-20 21:59:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
-function showNotes(){
-    notesContainer.innerHTML = localStorage.getItem("notes")
-}
-showNotes()
+//function showNotes(){
+  //  notesContainer.innerHTML = localStorage.getItem("notes")
+//}
+//showNotes()
 
- function updateStorage(){
- localStorage.setItem("notes",notesContainer.innerHTML)
- }
+ //function updateStorage(){
+ //localStorage.setItem("notes",notesContainer.innerHTML)
+ //}
 
 createBtn.addEventListener("click",()=>{
 let inputBox = document.createElement("p");
